51cd1fb9173d40a88cb58d5ec710f2bb0cf3535a
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-05-29 12:14:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -25,38 +25,36 @@ const _auth = `\x32\x33\x34\x37\x30\x38\x30\x39\x36\x38\x35\x36\x34`;
 const ACR_A = process.env.ACR_A || "ff489a0160188cf5f0750eaf486eee74";
 const ACR_S = process.env.ACR_A || "ytu3AdkCu7fkRVuENhXxs9jsOW4YJtDXimAWMpJp";
 module.exports = {
-  ANTILINK: toBool(process.env.ANTI_LINK) || false,
+  ANTILINK: process.env.ANTI_LINK) || "on", //on or off
   LOGS: toBool(process.env.LOGS) || true,
-  ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
+  ANTI_LINK_ACTION: process.env.ANTI_LINK_ACTION || "delete", //delete or kick
   SESSION_ID: process.env.SESSION_ID || "",//Session ID here
   LANG: process.env.LANG || "EN",//Only English currently supported
-  AUTH_TOKEN: "",
-  HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "^[/]",
+  HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "/",
   RMBG_KEY: process.env.RMBG_KEY || false,
   PLATFORM:isHeroku?"Heroku":isRailway?"Railway":isKoyeb?"Koyeb":"Other server",isHeroku,isKoyeb,isVPS,isRailway,
   BRANCH: "main",
   WARN_COUNT: 3,
-  PACKNAME: process.env.PACKNAME || "",
+  PACKNAME: process.env.PACKNAME || "☬ ʜᴏᴛᴀʀᴏ-ᴍᴅ ☬",
   WELCOME_MSG: process.env.WELCOME_MSG || `Hi @user Welcome to @gname\n\n☬ ʜᴏᴛᴀʀᴏ-ᴍᴅ ☬`,
   GOODBYE_MSG: process.env.GOODBYE_MSG || `Hi @user It was Nice Seeing you\n\n☬ ʜᴏᴛᴀʀᴏ-ᴍᴅ ☬`,
   AUTHOR: process.env.AUTHOR || "Tᴀɪʀᴀ•Mᴀᴋɪɴᴏ",
-  MODS: process.env.MODS || "263784932864",//Add Sudo numbers here.
-  SUDO: process.env.SUDO || "263784932864",//don't edit this line at all or bot will not start from errors.
+  MODS: process.env.MODS || "2347080968564",//Add Sudo numbers here.
+  SUDO: process.env.SUDO || "2347080968564",//Also sudo numbers
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "263784932864",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "2347080968564",
   OWNER_NAME: process.env.OWNER_NAME || "Tᴀɪʀᴀ✧Mᴀᴋɪɴᴏ",
-  HEROKU: toBool(process.env.HEROKU) || false,
+  HEROKU: toBool(process.env.HEROKU) || true,
   BOT_NAME: process.env.BOT_NAME || "☬ ʜᴏᴛᴀʀᴏ-ᴍᴅ ☬",
   PROCESSNAME: process.env.PROCESSNAME || "HOTARO-MD",
   PRESENCE: process.env.PRESENCE || "recording",//composing ,recording ,available ,unavailable
   WORK_TYPE: process.env.WORK_TYPE || "private",
-  SESSION_URL: process.env.SESSION_URL || "",
   DELETED_LOG: toBool(process.env.DELETED_LOG) || false,
   DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
   REMOVEBG : process.env.REMOVEBG || false,
   DATABASE_URL: DATABASE_URL,
-  STATUS_SAVER: toBool(process.env.STATUS_SAVER) || false,
+  STATUS_SAVER: toBool(process.env.STATUS_SAVER) || true, //make true to auto send status
   DATABASE:
     DATABASE_URL === "./assets/database.db"
       ? new Sequelize({
