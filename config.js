a10f77e5bc7e0b57ccebc1d13f88e7939881eb6f
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-06-05 11:16:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -24,6 +24,7 @@ const DATABASE_URL = process.env.DATABASE_URL || "./assets/database.db";
 const _auth = `\x32\x33\x34\x37\x30\x38\x30\x39\x36\x38\x35\x36\x34`;
 const ACR_A = process.env.ACR_A || "ff489a0160188cf5f0750eaf486eee74";
 const ACR_S = process.env.ACR_A || "ytu3AdkCu7fkRVuENhXxs9jsOW4YJtDXimAWMpJp";
+global.presence = process.env.PRESENCE || "recording",//composing ,recording ,available ,unavailable 
 module.exports = {
   ANTILINK: process.env.ANTI_LINK || "on", //on or off
   LOGS: toBool(process.env.LOGS) || true,
@@ -48,7 +49,6 @@ module.exports = {
   HEROKU: toBool(process.env.HEROKU) || true,
   BOT_NAME: process.env.BOT_NAME || "☬ ʜᴏᴛᴀʀᴏ-ᴍᴅ ☬",
   PROCESSNAME: process.env.PROCESSNAME || "HOTARO-MD",
-  PRESENCE: process.env.PRESENCE || "recording",//composing ,recording ,available ,unavailable
   WORK_TYPE: process.env.WORK_TYPE || "private",
   DELETED_LOG: toBool(process.env.DELETED_LOG) || false,
   DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
