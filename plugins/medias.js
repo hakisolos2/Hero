6a6c1f7f52ddc32f3122c3beb976349448e461d1
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-06-06 06:30:28 +0100

    Update medias.js

--- plugins/medias.js
+++ plugins/medias.js
@@ -1,5 +1,6 @@
 const { command, isPrivate, serialize } = require("../lib");
 const fs = require("fs");
+const { download } from 'aptoide-scraper'; 
 /*command({
             pattern: "play",
             fromMe: isPrivate,
@@ -34,3 +35,33 @@ const fs = require("fs");
             return message.sendMessage(message.jid, buttonMessage);
         }
         );*/
+
+command(
+{
+   pattern: "apk",
+   fromMe: isPrivate,
+   desc: "apk and mod apk download",
+   use: "downloader"
+}, async(message, match, m, client) => {   
+  try {
+      if (!match) return message.reply('*Please provide the APK Name to download.*')
+      await message.sendMessage(message.jid, {text: `*Downloading ${match}*`})
+      let data = await download(match);
+
+      if (data.size.replace(' MB', '') > 200) {
+        return await mesage.sendMessage(message.jid, { text: '*The file is too large,Cannot send.*' }, { quoted: message });
+      }
+
+      if (data.size.includes('GB')) {
+        return await message.sendMessage(message.jid, { text: '*The file is too large,cannot send..*' }, { quoted: message });
+      }
+      await message.sendMessage(
+        message.jid,
+        { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null },
+        { quoted: message }
+      )
+    }
+  } catch {
+    await message.reply(`*Download error occured.*`);
+  }
+};
